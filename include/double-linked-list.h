d2c1ecd4f11363d7fa4a4f7ab422b36e4717550d
Baibs FARARANO <baibsbelou@Baibs.local>
2022-02-17 14:28:29 +0300

    Collection: stack, queue and doubly linked list

--- include/double-linked-list.h
+++ include/double-linked-list.h
@@ -23,8 +23,76 @@ class DoubleLinkedList {
             delete this;
         }
 
-        void pushFirst(T value) {
-            
+        void addFirst(T value) {
+            if (this->isEmpty()) {
+                DoubleLinkNode<T>* tmp = new DoubleLinkNode<T>(value, this->head, this->tail);
+                this->tail = tmp;
+                this->head = tmp;
+            } else {
+                DoubleLinkNode<T>* tmp = new DoubleLinkNode<T>(value, this->head->getPrev(), this->head);
+                this->head->setPrev(tmp);
+                this->head = tmp;
+            }
+            this->size++;
+        }
+
+        void addLast(T value) {
+            if (this->isEmpty()) {
+                DoubleLinkNode<T>* tmp = new DoubleLinkNode<T>(value, this->head, this->tail);
+                this->tail = tmp;
+                this->head = tmp;
+            } else {
+                DoubleLinkNode<T>* tmp = new DoubleLinkNode<T>(value, this->tail, this->tail->getNext());
+                this->tail->setNext(tmp);
+                this->tail = tmp;
+            }
+            this->size++;
+        }
+
+        void removeFirst() {
+            if (this->isEmpty()) return;
+            DoubleLinkNode<T>* tmp = this->head;
+            this->setHead(this->head->getNext());
+            this->head->setPrev(tmp->getPrev());
+            free(tmp);
+            this->size--;
+        }
+
+        void removeLast() {
+            if (this->isEmpty()) return;
+            DoubleLinkNode<T>* tmp = this->tail;
+            this->setTail(this->tail->getPrev());
+            this->tail->setNext(tmp->getNext());
+            free(tmp);
+            this->size--;
+        }
+
+        void showForward() {
+            if (this->isEmpty()) {
+                std::cout << "Empty list" << std::endl;
+                return;
+            }
+            DoubleLinkNode<T>* tmp = this->head;
+            std::cout << "(" << this->size << ")[";
+            while (tmp != NULL) {
+                if (tmp->getNext() == NULL) std::cout << tmp->getValue() << "]" << std::endl;
+                else std::cout << tmp->getValue() << ", ";
+                tmp = tmp->getNext();
+            }
+        }
+
+        void showBackward() {
+            if (this->isEmpty()) {
+                std::cout << "Empty list" << std::endl;
+                return;
+            }
+            DoubleLinkNode<T>* tmp = this->tail;
+            std::cout << "(" << this->size << ")[";
+            while (tmp != NULL) {
+                if (tmp->getPrev() == NULL) std::cout << tmp->getValue() << "]" << std::endl;
+                else std::cout << tmp->getValue() << ", ";
+                tmp = tmp->getPrev();
+            }
         }
 
         DoubleLinkNode<T>* getHead() {
